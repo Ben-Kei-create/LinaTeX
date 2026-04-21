278271d345d7fbc7528ebb6b5191bbfdc4b1f43b
Ben-Kei-create <112257840+Ben-Kei-create@users.noreply.github.com>
2026-04-21 22:59:52 +0900

    Initial Commit

---
+++ LinaTeX/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LinaTeX
+//
+//  Created by 茂木史明 on 2026/04/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LinaTeX/LinaTeXApp.swift
@@ -0,0 +1,17 @@
+//
+//  LinaTeXApp.swift
+//  LinaTeX
+//
+//  Created by 茂木史明 on 2026/04/21.
+//
+
+import SwiftUI
+
+@main
+struct LinaTeXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
